4da0c7a3f98f3dfb82110d3bf04c13d996326cc9
Rutger Dijkstra <r.dijkstra@tfe.nl>
2024-06-06 09:38:10 +0000

    Umbraco 13 update

---
+++ Exceptions/ConfigurationException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace TFE.Umbraco.AccessRestriction.Exceptions
+{
+    [Serializable]
+    internal class ConfigurationException : Exception
+    {
+        public ConfigurationException()
+        {
+        }
+
+        public ConfigurationException(string? message) : base(message)
+        {
+        }
+
+        public ConfigurationException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ConfigurationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
